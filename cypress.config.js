4522127f68149f3fae9ab02c3abeab06bd528471
fawziridwan <fawziridwan@gmail.com>
2023-07-10 22:12:57 +0700

    update config

--- cypress.config.js
+++ cypress.config.js
@@ -1,28 +1,30 @@
 const { defineConfig } = require("cypress");
-// const createBundler = require("@bahmutov/cypress-esbuild-preprocessor");
-// const preprocessor = require("@badeball/cypress-cucumber-preprocessor");
-// const createEsbuildPlugin = require("@badeball/cypress-cucumber-preprocessor/esbuild");
+const cucumber = require('cypress-cucumber-preprocessor').default;
+const preprocessor = require("@badeball/cypress-cucumber-preprocessor");
 
-module.exports = defineConfig({
+module.exports = ({
   e2e: {
     setupNodeEvents(on, config) {
-      // on("file:preprocessor",
-      // createBundler({
-      //   plugins: [createEsbuildPlugin.default(config)],
-      // }));
-      // preprocessor.addCucumberPreprocessorPlugin(on, config);
-      // return config;
+      on("file:preprocessor", cucumber())
     },
-  
-  env:{
-    videoCompression: false, 
-    viewportWidth: 1280, 
-    viewportHeight: 720, 
-    defaultCommandTimeout:120000, 
-    requestTimeout : 30000, 
-    numTestsKeptInMemory: 0, 
-    responseTimeout : 50000, 
-    pageLoadTimeout: 120000,
-    }
-  },
-})
+    specPattern: "cypress/e2e/*.feature",
+    // baseUrl: "https://www.themoviedb.org",
+    // baseUrl: "https://www.saucedemo.com",
+    chromeWebSecurity: false,
+    // setupNodeEvents(on, config) {
+    //   on('file:preprocessor', cucumber())
+    //   return config;
+    // },
+    // specPattern: "**/*.feature",
+    // env: {
+    //   videoCompression: false,
+    //   viewportWidth: 1280,
+    //   viewportHeight: 720,
+    //   defaultCommandTimeout: 120000,
+    //   requestTimeout: 30000,
+    //   numTestsKeptInMemory: 0,
+    //   responseTimeout: 60000,
+      pageLoadTimeout: 70000,
+    // }
+  }
+});
